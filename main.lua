f8735bf1da669823d05831d642489c11daf98c8b
Myke Atkinson <ploe@hotmail.co.uk>
2017-12-04 22:12:49 +0000

    Controls added to timer. =)

--- main.lua
+++ main.lua
@@ -3,11 +3,18 @@ seconds = 00
 label = ""
 
 local i = 1
+local paused = true
 
 function love.load()
 	love.window.setMode(1080, 720)
 	love.graphics.setNewFont("digital-7 (mono).ttf", 256)
-	text_color(blue)
+
+	love.window.setTitle("Regular Expression")
+	local bruce = love.graphics.newImage("bruce.png")
+	love.window.setIcon(bruce:getData())
+
+	set_timer(states[i])
+	text_color(states[i])
 end
 
 function text_color(color)
@@ -19,45 +26,72 @@ function set_timer(color)
 	seconds = color.seconds
 end
 
+purple = {red=150, green=0, blue=255, minutes=0, seconds=5}
 green = {red=0, green=255, blue=0, minutes=2, seconds=0}
 amber = {red=255, green=194, blue=0, minutes=2, seconds=0}
 red = {red=255, green=0, blue=0, minutes=2, seconds=0}
-blue = {red=0, green=0, blue=255, minutes=0, seconds=30}
+blue = {red=0, green=255, blue=255, minutes=0, seconds=20}
 
 states = {
-	red,
+	purple,
+	green,
 	blue,
 	amber,
 	blue,
-	green,
+	red,
 }
 
 function love.keypressed(key, scancode, isrepeat)
 	if key == "escape" then
 		love.event.quit()
+	elseif (key == "space") or (key == " ") then
+		paused = not paused
+	elseif key == "left" then
+		if (i == 1) then return end
+
+		i = i - 1
+		set_timer(states[i])
+		text_color(states[i])
+	elseif key == "right" then
+		if (i == #states) then return end
+
+		i = i + 1
+		set_timer(states[i])
+		text_color(states[i])
 	end
 end
 
 function love.update(dt)
-	if minutes < 0 and seconds < 0 then return end
+	if (paused) then
+		label = string.format("%02d:%02d", minutes, seconds)
+		return 
+	end
+
+	if (i > #states) then 
+		label = string.format("end", minutes, seconds)
+		return
+	end
 
 	seconds = seconds - 1
 
-	if (seconds <= 0) then
+	if (seconds < 0) then
 		if (minutes > 0) then 
 			seconds = 59
 			minutes = minutes - 1
 		elseif (minutes <= 0) then
 			i = i + 1
+
+			if (i <= #states) then
+				set_timer(states[i])
+				text_color(states[i])
+			end
 		end
 	end
 
 	label = string.format("%02d:%02d", minutes, seconds)
-
 	love.timer.sleep(1)
 end
 
 function love.draw(dt)
 	love.graphics.printf(label, 0, 720/2, 1080, 'center')
 end
-
